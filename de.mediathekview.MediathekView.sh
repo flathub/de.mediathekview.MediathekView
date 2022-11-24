0a308d8a9f6a2803c6a94cfd08aa221f185b3880
Sebastian Wiesner <sebastian@swsnr.de>
2022-11-24 17:38:07 +0100

    Copy java options from appimage launcher

--- de.mediathekview.MediathekView.sh
+++ de.mediathekview.MediathekView.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-exec java -jar /app/share/de.mediathekview.MediathekView/MediathekView.jar "$@"
+# -XX flags come from the upstream appimage launcher, see
+# https://github.com/mediathekview/MediathekView/blob/master/scripte/appimage.sh
+#
+# -DexternalUpdateCheck disables the update check of mediathekview, because in
+# our case updates are distributed through flathub.
+exec java \
+    -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=compact -XX:MaxRAMPercentage=50.0 -XX:+UseStringDeduplication \
+    --add-opens java.desktop/sun.awt.X11=ALL-UNNAMED \
+    -DexternalUpdateCheck \
+    -jar /app/share/de.mediathekview.MediathekView/MediathekView.jar "$@"
