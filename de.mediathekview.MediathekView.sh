43561246de447f9b65599c4b98382b35afdb4adf
Sebastian Wiesner <sebastian@swsnr.de>
2025-06-06 09:47:14 +0200

    MediathekView 14.3.0
    The new release no longer builds a fat JAR, so we update the manifest
    to install all dependency JARs and extend the launch script to set an
    appropriate classpath.

--- de.mediathekview.MediathekView.sh
+++ de.mediathekview.MediathekView.sh
@@ -4,10 +4,16 @@
 #
 # -DexternalUpdateCheck disables the update check of mediathekview, because in
 # our case updates are distributed through flathub.
+
+basedir=/app/share/de.mediathekview.MediathekView
+
 exec java \
-    -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=compact -XX:MaxRAMPercentage=50.0 -XX:+UseStringDeduplication \
+    -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=compact \
+    -XX:MaxRAMPercentage=50.0 -XX:+UseStringDeduplication \
     --enable-native-access=ALL-UNNAMED --add-modules jdk.incubator.vector \
+    --add-exports=java.desktop/sun.swing=ALL-UNNAMED \
     --add-opens java.desktop/sun.awt.X11=ALL-UNNAMED \
+    -cp "${basedir}/MediathekView.jar:${basedir}/dependency/*" \
     -DexternalUpdateCheck \
-    -jar /app/share/de.mediathekview.MediathekView/MediathekView.jar \
+    mediathek.Main \
     "${XDG_CONFIG_HOME}/mediathek3"
